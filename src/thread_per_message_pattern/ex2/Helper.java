c941be8066cbe9d1b9e53f3a840c4f1ecd19de50
wapio <o2kbabata@gmail.com>
2017-09-04 00:27:31 +0900

    Executorsクラスを使用して得るThreadFactoryクラス

---
+++ src/thread_per_message_pattern/ex2/Helper.java
@@ -0,0 +1,25 @@
+package thread_per_message_pattern.ex2;
+
+public class Helper {
+	
+	public void handle(int count, char c) {
+		System.out.println("    handle(" + count + ", " + c + " ) BEGIN");
+		for (int i = 0; i < count; i++) {
+			slowly();
+			System.out.print(c);
+		}
+		System.out.println("");
+		System.out.println("    handle(" + count + ", " + c + " ) END");
+	}
+	
+	/**
+	 * <DT>スレッド待機</DT>
+	 */
+	private void slowly() {
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/thread_per_message_pattern/ex2/Main.java
@@ -0,0 +1,16 @@
+package thread_per_message_pattern.ex2;
+
+import java.util.concurrent.Executors;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("main BEGIN");
+		Host host = new Host(
+			Executors.defaultThreadFactory()
+		);
+		host.request(10, 'A');
+		host.request(20, 'B');
+		host.request(30, 'C');
+		System.out.println("main END");
+	}
+}
